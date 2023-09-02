c0eec6435c5b98bccd7a7a6e116935eb80aa5db5
Sassoricun <galactix.69vk256.1@gmail.com>
2023-09-02 23:08:17 +0300

    added time, changing icons

--- js/main.js
+++ js/main.js
@@ -1,136 +1,143 @@
-// Змінні для елементів сторінки
 const header = document.querySelector('.input')
 const form = document.querySelector('.search-box__search');
 const input = document.querySelector('.search-box__search-input');
 
 function removeCard() {
-  // Видаляю минулу карточку
   const prevCard = document.querySelector('.weather');
   if (prevCard) prevCard.remove();
 }
+function removeError() {
+  const prevError = document.querySelector('.error');
+  if (prevError) prevError.remove();
+}
 
-
-// "Прослуховуємо" відправку форми
 form.onsubmit = function (e) {
   e.preventDefault();
 
-  // Беремо значення з input
   let city = input.value;
 
-  // Робимо запит на сервер
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&key=3X23TEU5TAM2G85E5GNFRF7SX&contentType=json`;
 
-  //Виконуємо запит
-  fetch(url).then((response) => {
-    return response.json();
-  }).then((data) => {
-    // Перевірка на помилку
-    if (data.error) {
-      // Якщо є помилка - виводимо її
+  fetch(url)
+    .then((response) => { return response.json(); })
+    .then((data) => {
       removeCard();
-      // Відобразити вікно помилкі
-      const html = `<section class="error">
-                      <div class="container">
-                         <div class="error-box">
-                          <img class="error-box__img" src="./images/sad-icon.svg" alt="error images">
-                           <div class="error-box__text">
-                            <p class="error-box__title">Oooooops!</p>
-                            <p class="error-box__subtitle">Please enter some city and try again.</p>
+
+
+      let date = new Date();
+      let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+      let min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+
+      let days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+      let valueDay = date.getDay();
+
+
+      function lol() {
+        // let imgDay = '../images/big/sun-big.svg'
+        // let imgEvening = '../images/big/moon-big.svg'
+        // let imgNight = '../images/big/moon-big.svg'
+        if (hours >= 6 && hours < 18) {
+          console.log('sun')
+        } else if (hours >= 18 && hours < 24) {
+          console.log('evening')
+        } else if (hours >= 0 && hours < 6) {
+          console.log('night')
+        }
+      }
+
+
+      const filePathSmall = './images/small/' + (data.days[0].icon) + '.svg';
+      const filePathBig = './images/big/' + (data.days[0].icon) + '.svg';
+
+
+      const html = `<section class="weather">
+                      <div class="weather-box">
+                        <div class="weather-box__top">
+                          <img class="weather-box__state" src="${filePathSmall}" alt="weather state">
+                          <div class="weather-box__aside">
+                            <h3 class="weather-box__aside-title">${data.address}</h3>
+                            <p class="weather-box__aside-city">${data.resolvedAddress}</p>
+                            <p class="weather-box__aside-date">${data.days[0].datetime}</p>
+                          </div>
+                        </div>
+                        <div class="weather-box__temperature">
+                        <img class="weather-box__temperature-img" src="${filePathBig}" alt="">
+                          <p class="weather-box__temperature-big">${data.days[0].tempmax}°C</p>
+                          <p class="weather-box__temperature-state">${data.days[0].conditions}</p>
+                        </div>
+                        <div class="weather-box__details">
+                          <p class="weather-box__details-title">
+                            local weather report
+                          </p>
+                          <div class="weather-box__details-states">
+                            <div class="weather-box__details-states-box">
+                              <div class="weather-box__details-items">
+                                <ul class="weather-box__details-list">
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Feels like</p>
+                                    <p class="weather-box__details-prop">${data.days[0].feelslikemax + ' &degC'}</p>
+                                  </li>
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Sunrise</p>
+                                    <p class="weather-box__details-prop">${data.days[0].sunrise}</p>
+                                  </li>
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Sunset</p>
+                                    <p class="weather-box__details-prop">${data.days[0].sunset}</p>
+                                  </li>
+                                </ul>
+                              </div>
+                              <div class="weather-box__details-items">
+                                <ul class="weather-box__details-list">
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Humidity</p>
+                                    <p class="weather-box__details-prop">${data.days[0].humidity + ' %'}</p>
+                                  </li>
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Visibility</p>
+                                    <p class="weather-box__details-prop">${data.days[0].visibility + 'km'}</p>
+                                  </li>
+                                  <li class="weather-box__details-item">
+                                    <p class="weather-box__details-text">Pressure</p>
+                                    <p class="weather-box__details-prop">${data.days[0].pressure + ' GPa'}</p>
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                            <div class="weather-box__details-aside">
+                              <img class="weather-box__details-img" src="${lol()}" alt="">
+                              <p class="weather-box__details-time">${hours}:${min}</p>
+                              <p class="weather-box__details-day">${days[valueDay]}</p>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </section>`;
-
-      // Відображаємо карточку на сторінці
+      removeError()
       header.insertAdjacentHTML('afterend', html);
-
-    } else {
-      // Якщо помилки не має - виводимо карточку
-
-      // Відображаю отриманні данні в карточці
-      // Розмітка для карточки
-      const html = `<section class="weather">
-                    <div class="weather-box">
-                      <div class="weather-box__top">
-                        <img class="weather-box__state" src="./images/smal/cloud-small.svg" alt="weather state">
-                        <div class="weather-box__aside">
-                          <h3 class="weather-box__aside-title">${data.address}</h3>
-                          <p class="weather-box__aside-city">${data.resolvedAddress}</p>
-                          <p class="weather-box__aside-date">${data.days[0].datetime}</p>
-                        </div>
-                      </div>
-                      <div class="weather-box__temperature">
-                        <p class="weather-box__temperature-big">${data.days[0].tempmax}°C</p>
-                        <p class="weather-box__temperature-state">${data.days[0].conditions}</p>
-                      </div>
-                      <div class="weather-box__details">
-                        <p class="weather-box__details-title">
-                          local weather report
-                        </p>
-                        <div class="weather-box__details-states">
-                          <div class="weather-box__details-states-box">
-                            <div class="weather-box__details-items">
-                              <ul class="weather-box__details-list">
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Feels like</p>
-                                  <p class="weather-box__details-prop">${data.days[0].feelslikemax}°C</p>
-                                </li>
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Sunrise</p>
-                                  <p class="weather-box__details-prop">${data.days[0].sunrise}</p>
-                                </li>
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Sunset</p>
-                                  <p class="weather-box__details-prop">${data.days[0].sunset}</p>
-                                </li>
-                              </ul>
-                            </div>
-                            <div class="weather-box__details-items">
-                              <ul class="weather-box__details-list">
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Humidity</p>
-                                  <p class="weather-box__details-prop">${data.days[0].humidity}</p>
-                                </li>
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Visibility</p>
-                                  <p class="weather-box__details-prop">${data.days[0].visibility}</p>
-                                </li>
-                                <li class="weather-box__details-item">
-                                  <p class="weather-box__details-text">Pressure</p>
-                                  <p class="weather-box__details-prop">${data.days[0].pressure}</p>
-                                </li>
-                              </ul>
+    })
+    .catch(() => {
+      removeCard();
+      const html = `<section class="error">
+                        <div class="container">
+                           <div class="error-box">
+                            <img class="error-box__img" src="./images/sad-icon.svg" alt="error images">
+                             <div class="error-box__text">
+                              <p class="error-box__title">Oooooops!</p>
+                              <p class="error-box__subtitle">Please enter some city and try again.</p>
                             </div>
                           </div>
-                          <div class="weather-box__details-aside">
-                            <img class="weather-box__details-img" src="./images/big/moon-big.svg" alt="">
-                            <p class="weather-box__details-time">1:29</p>
-                            <p class="weather-box__details-day">Friday</p>
-                          </div>
                         </div>
-                      </div>
-                    </div>
-                  </section>`;
-
-      // Відображаємо карточку на сторінці
+                      </section>`;
 
       header.insertAdjacentHTML('afterend', html);
-    }
-
-    // console.log(data.address);
-    // console.log(data.resolvedAddress);
-    // console.log(data.days[0].datetime);
-
-    // console.log(data.days[0].tempmax);
-    // console.log(data.days[0].conditions);
-
-    // console.log(data.days[0].feelslikemax);
-    // console.log(data.days[0].sunrise);
-    // console.log(data.days[0].sunset);
-
-    // console.log(data.days[0].humidity);
-    // console.log(data.days[0].visibility);
-    // console.log(data.days[0].pressure);
-
-  });
+    })
 }
